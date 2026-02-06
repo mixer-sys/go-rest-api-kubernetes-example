de358edf0938e8add6e05ec93151f34355117501
Your Name <you@example.com>
2026-02-06 12:01:26 +0300

    Graceful shutdown done

---
+++ cmd/app/main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func newRouter() *httprouter.Router {
+	mux := httprouter.New()
+
+	mux.GET("/youtube/channel/stats", getChannelStats())
+
+	return mux
+}
+
+func getChannelStats() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		w.Write([]byte("response!"))
+	}
+
+}
+
+func main() {
+
+	srv := &http.Server{
+		Addr:    ":10101",
+		Handler: newRouter(),
+	}
+
+	idleConsClosed := make(chan struct{})
+
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		signal.Notify(sigint, syscall.SIGTERM)
+		<-sigint
+		log.Println("service interrupt received")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("http server shutdown error: %v", err)
+
+		}
+		log.Println("shutdown complete")
+
+		close(idleConsClosed)
+	}()
+
+	if err := srv.ListenAndServe(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("fatal http server failed to start: %v", err)
+		}
+	}
+
+	<-idleConsClosed
+
+	log.Println("Service Stop")
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/mixer-sys/go-rest-api-kubernetes-example
+
+go 1.25.3
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
